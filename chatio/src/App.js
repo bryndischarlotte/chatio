5e3f3873f2dde2b9c6c32045975e1c2d34cc721c
brynjargauti <brynjarthorsteins@gmail.com>
2019-03-20 21:50:38 +0000

    create new room

--- chatio/src/App.js
+++ chatio/src/App.js
@@ -1,27 +1,12 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { socket } from './services/socketService';
 import LoginWindow from './components/LoginWindow/LoginWindow';
 import Lobby from './components/Lobby/Lobby';
 
 class App extends Component {
-    componentDidMount() {
-        socket.emit('users');
-        socket.on('userlist', userList => {
-            this.setState({users: userList});
-        })
-    }
-    constructor(props) {
-        super(props);
-        this.state = {
-            users: []
-        };
-    }
     render() {
         return (
             <div>
-                {console.log(socket)}
-                {console.log(this.state)}
                 <Switch>
                     <Route exact path="/" component={ LoginWindow } />
                     <Route exact path="/lobby" component={ Lobby } />
