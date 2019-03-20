652c2b860c75404dfba53241695f28846171af69
brynjargauti <brynjarthorsteins@gmail.com>
2019-03-20 14:35:02 +0000

    app modify

--- chatio/src/App.js
+++ chatio/src/App.js
@@ -5,12 +5,30 @@ import LoginWindow from './components/LoginWindow/LoginWindow';
 
 class App extends Component {
     componentDidMount() {
-        socket.on()
+        socket.emit('adduser', "username2", (inp) => {
+            if (inp === true) {
+                console.log("works");
+            }
+            else {
+                console.log("doesn't work");
+            }
+        })
+        socket.emit('users');
+        socket.on('userlist', userList => {
+            this.setState({users: userList});
+        })
+    }
+    constructor(props) {
+        super(props);
+        this.state = {
+            users: []
+        };
     }
     render() {
         return (
             <div>
                 {console.log(socket)}
+                {console.log(this.state)}
                 <Switch>
                     <Route exact path="/" component={LoginWindow} />
                 </Switch>
