ad69440356157e68e98540fe5daec11d6ee6f963
brynjargauti <brynjarthorsteins@gmail.com>
2019-03-21 12:04:50 +0000

    roomview + roomview item

---
+++ chatio/src/components/RoomViewItem/RoomViewItem.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import Modal from 'react-bootstrap4-modal';
+import { socket } from '../../services/socketService';
+
+class RoomViewItem extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            password: '',
+            showModal: false
+        };
+    }
+    toggleModal(inp) {
+        if (inp === true) {
+            this.setState({ ...this.state, showModal: true });
+        }
+        else {
+            this.setState({ ...this.state, showModal: false });
+        }
+    }
+    roomPrivacy(priv) {
+        if (priv) {
+            return 'Private';
+        }
+        else {
+            return 'Public';
+        }
+    }
+    tryEnterRoom(priv) {
+        if (priv) {
+            this.toggleModal(true);
+        }
+        else {
+            this.enterRoom('', false);
+        }
+    }
+    enterRoom (password, priv) {
+        const roomObj = {
+            room: this.props.name,
+            pass: undefined
+        }
+        if (priv) {
+            if (password === '') { return false; }
+            roomObj.pass = password;
+        }
+        socket.emit('joinroom', roomObj, (resp) => {
+            if (resp === true) {
+                socket.emit('rooms');
+                this.toggleModal(false);
+            }
+            else {
+                console.log('Did not work');
+            }
+        });
+        this.setState({ ...this.state, password: '' });
+    }
+    render() {
+        const { room, name } = this.props;
+        const { password, showModal } = this.state;
+        return (
+            <div>
+                <h3 ><strong>{name}</strong></h3>
+                <p>Users in room: {Object.keys(room.users).length}</p>
+                <p>Topic: {room.topic}</p>
+                <p>{this.roomPrivacy(room.locked)}</p>
+                <button type="button" className="btn btn-success" onClick={() => this.tryEnterRoom(room.locked)} >Join Room</button>
+                <Modal visible={ showModal }>
+                    <div className="modal-header">
+                        <h5 className="modal-title">Enter password</h5>
+                    </div>
+                    <div className="modal-body">
+                        <label className="control-label" htmlFor="password">PASSWORD:</label>
+                        <input type="text" name={name} id={`password-${name}`} className="form-control" value={ password } 
+                        onChange={e => this.setState({ ...this.state, password: e.target.value })} />
+                    </div>
+                    <div className="modal-footer">
+                        <button type="button" className="btn btn-secondary" onClick={() => this.toggleModal(false)}>
+                            Cancel
+                </button>
+                        <button type="button" className="btn btn-primary" onClick={() => this.enterRoom(password, true)}>
+                            Submit
+                </button>
+                    </div>
+                </Modal>
+            </div>
+        );
+    }
+};
+
+export default RoomViewItem;
